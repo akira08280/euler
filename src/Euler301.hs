2250322e197a0f89e97e711616ec9fd63a85ea7a
Akira Yano <akira.08280@gmail.com>
2017-10-22 11:59:13 +0900

    remove unnecessary comment

--- src/Euler301.hs
+++ src/Euler301.hs
@@ -9,6 +9,5 @@ import Data.Bits (xor)
 e301_solve :: Int
 e301_solve = length . filter nim $ [1..2 ^ 30]
 
--- nim :: Integral a => a -> Bool
 nim :: Int -> Bool
 nim n = n `xor` n * 2 `xor` n * 3 == 0

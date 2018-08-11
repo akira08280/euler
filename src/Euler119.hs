96b26212099b0ccd73b7a558b5a81568ea458b9a
Akira Yano <akira.08280@gmail.com>
2018-08-11 12:11:52 +0900

    add euler 119 solution

---
+++ src/Euler119.hs
@@ -0,0 +1,24 @@
+{--
+  https://projecteuler.net/problem=119
+--}
+
+module Euler119 (e119_solve) where
+
+import Data.Char (digitToInt)
+import Data.List (sort)
+import Control.Monad (guard)
+
+e119_solve :: Integer
+e119_solve = (sort digitPowerSums) !! 29
+
+digitPowerSums :: [Integer]
+digitPowerSums = do
+  b <- [2..100]
+  e <- [2..10]
+  let
+    t = b ^ e
+  guard (sumOfDigit t == b)
+  return t
+
+sumOfDigit :: Show a => a -> Integer
+sumOfDigit = toInteger . sum . map digitToInt . show

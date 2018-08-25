0c191e119302de77048b722838dbb2323600fc70
Akira Yano <akira.08280@gmail.com>
2018-08-25 15:15:46 +0900

    refactor

--- src/Euler122.hs
+++ src/Euler122.hs
@@ -19,9 +19,8 @@ e122_solve = V.sum . depthAddChain 2 baseBranch $ baseMins
 
 depthAddChain :: (Num a, Ord a, Enum a) => a -> [Int] -> V.Vector a -> V.Vector a
 depthAddChain 12 branch mins = mins
-depthAddChain  d branch mins = foldl step mins nextExps
+depthAddChain  d branch mins = foldl step mins $ nextExps branch
   where
-    nextExps = nub . filter (> head branch) . liftM2 (+) branch $ branch
     step da e
       | i >= maxExp = da
       | otherwise = case compare (da V.! i) d of
@@ -29,10 +28,13 @@ depthAddChain  d branch mins = foldl step mins nextExps
                       EQ -> depthAddChain (succ d) (e:branch) da
                       LT -> da
       where
-        i = pred e
+        i = pred e -- Index of Data.Array.Diff starts from 1, but Vector's index starts from 0
+
+nextExps :: [Int] -> [Int]
+nextExps current = nub . filter (> head current) . liftM2 (+) current $ current
 
 baseBranch :: [Int]
-baseBranch = [1,2]
+baseBranch = [2,1]
 
 baseMins :: V.Vector Int
 baseMins = V.fromListN maxExp $ 0:1:repeat maxBound

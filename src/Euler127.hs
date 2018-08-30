2651015cd51792679db5f28137c660d83408d26c
Akira Yano <akira.08280@gmail.com>
2018-08-30 22:19:44 +0900

    add euler 127 explain

--- src/Euler127.hs
+++ src/Euler127.hs
@@ -1,5 +1,38 @@
 {--
   https://projecteuler.net/problem=127
+
+  The radical of n, rad(n), is the product of distinct prime factors of n.
+  For example, 504 = 23 x 32 x 7, so rad(504) = 2 x 3 x 7 = 42.
+  We shall define the triplet of positive integers (a, b, c) to be an abc-hit if:
+
+  1. GCD(a, b) = GCD(a, c) = GCD(b, c) = 1
+  2. a < b
+  3. a + b = c
+  4. rad(abc) < c
+
+  Insight (1)
+
+    We have defined the following relationship a + b = c.
+    Then If and only if GCD(a,b) = 1 the it follows that GCD(a, c) = GCD(b, c) = 1.
+    GCD(a, c) = GCD(a, a + b)
+    This means (a + b) / d = a / d + b / d. But since GCD(a, b) = 1, there is no d which divides a and b.
+    So we only need to check GCD(a, b).
+
+  Insight (2)
+
+    If GCD(a, b) = 1 then we have that rad(a) * rad(b) * rad(c) = rad(abc).
+    Because GCD(a, b) = GCD(a, c) = GCD(b, c) = 1, so a, b and c do not have a common divisor.
+
+  Insight (3)
+
+    Since a < b, rad(b) > 2.
+    Since rad(a) * rad(b) * rad(c) < c, rad(a) * rad(c) < c / 2.
+    Therefore, you do not need to check the following cases.
+      -> rad(a) * rad(c) >= c / 2
+
+  Insight (4)
+
+    Since a + b = c and a < b, a < c / 2.
 --}
 
 module Euler127 (e127_solve) where
@@ -28,7 +61,7 @@ abchits = do
     rada = snd t
     radb = snd $ vRads V.! (fromIntegral $ b - 1)
   guard (rada * radb * radc < c)
-  guard (gcd rada radb == 1)
+  guard (gcd a b == 1)
   return c
   where
     vRads = V.fromList . rads $ limit

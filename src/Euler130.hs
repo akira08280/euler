c26c5e8e8d46f59277c094deb86ae6f414e14d4c
Akira Yano <akira.08280@gmail.com>
2018-09-04 15:05:41 +0900

    fix comment

--- src/Euler130.hs
+++ src/Euler130.hs
@@ -3,11 +3,7 @@
 
   Due to the nature of A(n), R(A(n)) is divisible by n.
   If (n - 1) is divisible by A(n), R(n - 1) can also be divisible by n.
-
-  R(n - 1) = (10^(n - 1) - 1) / 9.
-
-  Since R(n - 1) can also be divisible by n, (10^(n - 1) - 1) / 9n.
-  So 10^(n - 1) = 1 (mod 9n)
+  Because n - 1 is larger than A(n) and R(n - 1) is multiple of R(A(n)), R(A(n)) is divisible by n. Therefore R(n - 1) is divisible by n.
 
   -- Repunit Wikipedia
     https://en.wikipedia.org/wiki/Repunit#Properties
@@ -15,6 +11,11 @@
   -- Exanple
     R(35) is divisible by R(7), because 35 is divisible by 7.
     11111111111111111111111111111111111 `divMod` 1111111 = (10000001000000100000010000001, 0)
+
+  R(n - 1) = (10^(n - 1) - 1) / 9.
+
+  Since R(n - 1) can also be divisible by n, (10^(n - 1) - 1) / 9n.
+  So 10^(n - 1) = 1 (mod 9n)
 --}
 
 module Euler130 (e130_solve) where

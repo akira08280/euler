a212aa34cbd1eb4224b1a83f00b7ac6d0c77c84a
Akira Yano <akira.08280@gmail.com>
2018-09-04 17:00:57 +0900

    refactor

--- src/Euler132.hs
+++ src/Euler132.hs
@@ -1,14 +1,14 @@
 {--
   https://projecteuler.net/problem=132
 
-  When p is a prime factor of R(10^9), the following expression holds.
+  When p is a prime factor of R(10 ^ 9), the following expression holds.
 
   10 ^ (10 ^ 9) - 1 / 9 = 0 (mod p)
   10 ^ (10 ^ 9) - 1 = 0 (mod p) 
   10 ^ (10 ^ 9) = 1 (mod p)
 
   Since 2 and 3 can not be prime factors, we check from prime numbers of 5 or more.
-  2 is trivial, because 3 does not become a multiple of 3 even if adding 1 to 10^9.
+  2 is trivial, because 3 does not become a multiple of 3 even if adding 1 to 10 ^ 9.
 --}
 
 module Euler132 (e132_solve) where

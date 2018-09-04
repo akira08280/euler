cea95dbbdedf59431f8c0e87328a595b05c3d39f
Akira Yano <akira.08280@gmail.com>
2018-09-04 16:23:05 +0900

    refactor

--- src/Euler129.hs
+++ src/Euler129.hs
@@ -7,10 +7,19 @@
 
 module Euler129 (e129_solve) where
 
-import Repunit (a)
-
 e129_solve :: Int
 e129_solve = head . filter (\n -> a n > limit) $ [limit + 1, limit + 3..]
+
+a :: Integral a => a -> a
+a n
+  | gcd n 10 > 1 = 0
+  | otherwise = findDivisible 1 2
+  where
+    findDivisible m k
+      | m' == 0 = k
+      | otherwise = findDivisible m' (succ k)
+      where
+        m' = (m * 10 + 1) `mod` n -- Principle of calculating a remainder with pen and paper
 
 limit :: Integral a => a
 limit = 10 ^ 6

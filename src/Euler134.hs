cb7b14e33cc53cd2ff012f29cbcdd959fadbd58b
Akira Yano <akira.08280@gmail.com>
2018-09-07 14:38:50 +0900

    add explain euler 134

--- src/Euler134.hs
+++ src/Euler134.hs
@@ -1,5 +1,11 @@
 {--
   https://projecteuler.net/problem=134
+
+  For example, in the case of 19 and 23.
+  Divide by 100 to find the reminder 19 and the number divisible by 23 with the Chinese Remainder Theorem.
+
+  1219 = 0  (mod 23)
+  1219 = 19 (mod 100)
 --}
 
 module Euler134 (e134_solve) where
@@ -11,7 +17,7 @@ import Math.NumberTheory.Primes.Sieve (primeList, primeSieve)
 e134_solve :: Integer
 e134_solve = sum . map smallest $ [2..(length primes - 2)]
   where
-    primes = sieve $ limit + 4
+    primes = sieve $ limit + 4 -- last prime's pair is (999983, 1000003)
     smallest n = chineseRemainder2 (p1, 10 ^ (digit $ p1)) (0, p2)
       where
         p1 = primes !! n

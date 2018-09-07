231b7bfbd260a396d9805af1b7f953caf50dc44f
Akira Yano <akira.08280@gmail.com>
2018-09-07 14:19:42 +0900

    add euler 134 solution

---
+++ src/Euler134.hs
@@ -0,0 +1,24 @@
+{--
+  https://projecteuler.net/problem=134
+--}
+
+module Euler134 (e134_solve) where
+
+import Common (digit)
+import Math.NumberTheory.Moduli.Chinese (chineseRemainder2)
+import Math.NumberTheory.Primes.Sieve (primeList, primeSieve)
+
+e134_solve :: Integer
+e134_solve = sum . map smallest $ [2..(length primes - 2)]
+  where
+    primes = sieve $ limit + 4
+    smallest n = chineseRemainder2 (p1, 10 ^ (digit $ p1)) (0, p2)
+      where
+        p1 = primes !! n
+        p2 = primes !! (succ n)
+
+sieve :: Integer -> [Integer]
+sieve = primeList . primeSieve
+
+limit :: Integral a => a
+limit = 10 ^ 6

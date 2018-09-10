b86d20d169c3f0ff117a1472bbd0263ea9234e9b
Akira Yano <akira.08280@gmail.com>
2018-09-10 14:18:48 +0900

    fix hlint

--- src/Euler117.hs
+++ src/Euler117.hs
@@ -9,4 +9,4 @@ module Euler117 (e117_solve) where
 e117_solve :: Integer
 e117_solve = last . take 51 . map (\(_, _, _, d) -> d) . iterate loop $ (0, 0, 0, 1)
   where
-    loop (a, b, c, d) = (b, c, d, (a + b + c + d))
+    loop (a, b, c, d) = (b, c, d, a + b + c + d)

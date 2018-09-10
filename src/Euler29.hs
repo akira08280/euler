e6521851daccb3318118b7ebea281cc625fdf60e
Akira Yano <akira.08280@gmail.com>
2018-09-10 11:37:02 +0900

    fix hlint

--- src/Euler29.hs
+++ src/Euler29.hs
@@ -7,4 +7,4 @@ module Euler29 (e29_solve) where
 import qualified Data.Set as Set (fromList)
 
 e29_solve :: Int
-e29_solve = length . Set.fromList $ (\a b -> a ^ b) <$> [2..100] <*> [2..100]
+e29_solve = length . Set.fromList $ (^) <$> [2..100] <*> [2..100]

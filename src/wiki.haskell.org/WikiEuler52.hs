0b9f56af704109853bd45f88ecd5d1ae5ed12049
Akira Yano <akira.08280@gmail.com>
2018-09-10 15:30:59 +0900

    fix hlint

--- src/wiki.haskell.org/WikiEuler52.hs
+++ src/wiki.haskell.org/WikiEuler52.hs
@@ -6,8 +6,8 @@ module WikiEuler52 (wiki_e52_solve) where
 
 import Data.List
  
-has_same_digits a b = (show a) \\ (show b) == []
+hasSameDigits a b = null (show a \\ show b)
   
-check n = all (has_same_digits n) (map (n*) [2..6])
+check n = all (hasSameDigits n) (map (n*) [2..6])
    
 wiki_e52_solve = head $ filter check [1..]

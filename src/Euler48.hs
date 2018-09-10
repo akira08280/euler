d98d50f69d565ecaf1ed753c99e3d54054dc1230
Akira Yano <akira.08280@gmail.com>
2018-09-10 12:40:57 +0900

    fix hlint

--- src/Euler48.hs
+++ src/Euler48.hs
@@ -5,4 +5,4 @@
 module Euler48 (e48_solve) where
 
 e48_solve :: Integral a => a
-e48_solve = mod (sum . zipWith (^) [1..10 ^ 3] $ [1..10 ^ 3]) $ (10 ^ 10)
+e48_solve = mod (sum . zipWith (^) [1..10 ^ 3] $ [1..10 ^ 3]) (10 ^ 10)

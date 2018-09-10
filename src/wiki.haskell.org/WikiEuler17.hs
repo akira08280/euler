4bed1d6da1da1e2c8debd2b37ccc11663045cd64
Akira Yano <akira.08280@gmail.com>
2018-09-10 15:00:59 +0900

    fix hlint

--- src/wiki.haskell.org/WikiEuler17.hs
+++ src/wiki.haskell.org/WikiEuler17.hs
@@ -12,12 +12,12 @@ ty = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
 
 decompose x 
   | x == 0                       = []
-  | x < 20                       = one !! (x-1)
-  | x >= 20 && x < 100           = ty !! (firstDigit (x) - 2) ++ decompose ( x - firstDigit (x) * 10)
-  | x < 1000 && x `mod` 100 ==0  = one !! (firstDigit (x)-1) ++ "hundred"
-  | x > 100 && x <= 999          = one !! (firstDigit (x)-1) ++ "hundredand" ++ decompose ( x - firstDigit (x) * 100)
+  | x < 20                       = one !! (x - 1)
+  | x >= 20 && x < 100           = ty !! (firstDigit x - 2) ++ decompose (x - firstDigit x * 10)
+  | x < 1000 && x `mod` 100 ==0  = one !! (firstDigit x - 1) ++ "hundred"
+  | x > 100 && x <= 999          = one !! (firstDigit x - 1) ++ "hundredand" ++ decompose (x - firstDigit x * 100)
   | x == 1000                    = "onethousand"
   where
-    firstDigit x = digitToInt . head . show $ x
+    firstDigit = digitToInt . head . show
 
 wiki_e17_solve = length . concatMap decompose $ [1..1000]

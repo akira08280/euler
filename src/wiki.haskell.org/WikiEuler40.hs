54d96efd5534545bac41671f5b434d85210fa170
Akira Yano <akira.08280@gmail.com>
2018-09-10 15:04:51 +0900

    fix hlint

--- src/wiki.haskell.org/WikiEuler40.hs
+++ src/wiki.haskell.org/WikiEuler40.hs
@@ -4,8 +4,9 @@
 
 module WikiEuler40 (wiki_e40_solve) where
 
-import Data.Char
+import Data.Char (digitToInt)
 
-wiki_e40_solve = (d 1)*(d 10)*(d 100)*(d 1000)*(d 10000)*(d 100000)*(d 1000000)
-  where n = concat [show n | n <- [1..]]
-        d j = Data.Char.digitToInt (n !! (j-1))
+wiki_e40_solve = d 1 * d 10 * d 100 * d 1000 * d 10000 * d 100000 * d 1000000
+  where
+    n = concat [show n | n <- [1..]]
+    d j = digitToInt (n !! (j - 1))

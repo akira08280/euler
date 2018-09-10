a2967b59863e355714253d38f5b4eeeda24558e9
Akira Yano <akira.08280@gmail.com>
2018-09-10 11:35:26 +0900

    fix hlint

--- src/Euler28.hs
+++ src/Euler28.hs
@@ -5,4 +5,4 @@
 module Euler28 (e28_solve) where
 
 e28_solve :: Integral a => a
-e28_solve = succ . sum . map (\n -> (4 * (2 * n + 1) ^ 2 - 12 * n)) $ [1..500]
+e28_solve = succ . sum . map (\n -> 4 * (2 * n + 1) ^ 2 - 12 * n) $ [1..500]

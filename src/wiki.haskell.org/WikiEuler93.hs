371be753a76f1a0a378a9fd59ceea2ce2ca887dc
Akira Yano <akira.08280@gmail.com>
2018-09-10 15:09:21 +0900

    fix hlint

--- src/wiki.haskell.org/WikiEuler93.hs
+++ src/wiki.haskell.org/WikiEuler93.hs
@@ -12,27 +12,26 @@ solve [] [x] = [x]
 solve ns stack = 
   pushes ++ ops
   where
-  pushes = do
-    x <- ns
-    solve (x `delete` ns) (x:stack)
-  ops = do
-    guard (length stack > 1)
-    x <- opResults (stack!!0) (stack!!1)
-    solve ns (x : drop 2 stack)
+    pushes = do
+      x <- ns
+      solve (x `delete` ns) (x:stack)
+    ops = do
+      guard (length stack > 1)
+      x <- opResults (head stack) (stack !! 1)
+      solve ns (x : drop 2 stack)
 
-opResults a b = 
-  [a*b,a+b,a-b] ++ (if b /= 0 then [a / b] else [])
+opResults a b = [a * b, a + b, a - b] ++ [a / b | b /= 0]
 
 results xs = fun 1 ys
   where
   ys = nub $ sort $ map truncate $ filter (\x -> x > 0 && floor x == ceiling x) $ solve xs [] 
   fun n (x:xs) 
-    |n == x =fun (n+1) xs 
-    |otherwise=n-1
+    | n == x = fun (n + 1) xs
+    | otherwise = n - 1
 
 cmp = comparing results
 
-wiki_e93_solve = maximumBy cmp $ [[a,b,c,d] | a <- [1..10], 
-                                              b <- [a+1..10], 
-                                              c <- [b+1..10], 
-                                              d <- [c+1..10]]
+wiki_e93_solve = maximumBy cmp [[a,b,c,d] | a <- [1..10],
+                                            b <- [a + 1..10],
+                                            c <- [b + 1..10],
+                                            d <- [c + 1..10]]

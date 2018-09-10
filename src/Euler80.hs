bd8d14a25cf65f4d7478f8c566bd55e9552ac5b0
Akira Yano <akira.08280@gmail.com>
2018-09-10 13:13:47 +0900

    fix hlint

--- src/Euler80.hs
+++ src/Euler80.hs
@@ -12,7 +12,7 @@ e80_solve :: Int
 e80_solve = sum . map (sum . map digitToInt . show . flip sqrtlong 100) . filter (not . isSquare) . enumFromTo 1 $ 100
 
 sqrtlong :: Integral a => a -> a -> a
-sqrtlong n d = flip div 100 . snd . head . dropWhile ((< 10 ^ (succ d)) . snd) . iterate sqrtlong' $ (5 * n, 5)
+sqrtlong n d = flip div 100 . snd . head . dropWhile ((< 10 ^ succ d) . snd) . iterate sqrtlong' $ (5 * n, 5)
   where
     sqrtlong' (a, b)
       | a >= b = (,) ((+a) . negate $ b) ((+ 10) b)

194eb4d84d47cbbfd856447c04faa170fc50c007
Akira Yano <akira.08280@gmail.com>
2018-09-10 12:16:29 +0900

    fix hlint

--- src/Euler31.hs
+++ src/Euler31.hs
@@ -19,6 +19,6 @@ dp = runSTUArray $ do
     coins = [1,2,5,10,20,50,100,200]
   ways <- newArray (0, target) 0
   unsafeWrite ways 0 1
-  forM_ coins $ \coin -> do
+  forM_ coins $ \coin ->
     mapM_ (\j -> unsafeRead ways j >>= (\k -> unsafeRead ways (j-coin) >>= (\l -> unsafeWrite ways j (l+k)))) [coin..target]
   return ways

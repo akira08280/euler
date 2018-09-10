ad6864186c3066fe7e332a11edef2bba49966761
Akira Yano <akira.08280@gmail.com>
2018-09-10 14:10:32 +0900

    fix hlint

--- src/Euler102.hs
+++ src/Euler102.hs
@@ -17,11 +17,11 @@ e102_solve = do
   file <- readFile "src/resources/p102_triangles.txt"
   let
     triangles = map (\line -> read ("[" ++ line ++ "]") :: [Int]) . lines $ file
-  return . length . filter (\t -> t) . map includeOrigin $ triangles
+  return . length . filter id . map includeOrigin $ triangles
 
 includeOrigin :: [Int] -> Bool
 includeOrigin ns = all (< 0) [v1, v2, v3] || all (> 0) [v1, v2, v3]
   where
-    v1 = (ns !! 0 * ns !! 3) - (ns !! 1 * ns !! 2)
+    v1 = (head ns * ns !! 3) - (ns !! 1 * ns !! 2)
     v2 = (ns !! 2 * ns !! 5) - (ns !! 3 * ns !! 4)
-    v3 = (ns !! 4 * ns !! 1) - (ns !! 5 * ns !! 0)
+    v3 = (ns !! 4 * ns !! 1) - (ns !! 5 * head ns)

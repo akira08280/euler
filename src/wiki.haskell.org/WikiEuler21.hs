2ceca3cd2864235b1fa899258d0b76396ff781d1
Akira Yano <akira.08280@gmail.com>
2018-09-10 15:02:45 +0900

    fix hlint

--- src/wiki.haskell.org/WikiEuler21.hs
+++ src/wiki.haskell.org/WikiEuler21.hs
@@ -9,8 +9,8 @@ import Data.Array
 max_ = 100000
 
 gen 100001 = []
-gen n = [(i*n,n)|i <- [2 .. max_ `div` n]] ++ (gen (n+1))
+gen n = [(i*n,n)|i <- [2 .. max_ `div` n]] ++ gen (n + 1)
 
-arr = accumArray (+) 0 (0,max_) (gen 1)
+arr = accumArray (+) 0 (0, max_) (gen 1)
 
-wiki_e21_solve = sum $ filter (\a -> let b = (arr!a) in b /= a && (arr!b) == a) [1 .. (10000 - 1)]
+wiki_e21_solve = sum $ filter (\a -> let b = (arr ! a) in b /= a && (arr ! b) == a) [1..(10000 - 1)]

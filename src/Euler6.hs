dabb12c181e33d42d06760a7f83e3378d4d0117c
Akira Yano <akira.08280@gmail.com>
2018-09-10 11:09:16 +0900

    fix hlint

--- src/Euler6.hs
+++ src/Euler6.hs
@@ -5,9 +5,13 @@
 module Euler6 (e6_solve) where
 
 e6_solve :: Integral a => a
-e6_solve = round result
-  where
-    n = 100
-    sum = (\n -> n * (n + 1) / 2)
-    square = (\n -> (n * (n + 1) * (2 * n + 1)) / 6)
-    result = (sum n) ^ 2 - square n
+e6_solve = round $ sumOfSquares limit ^ 2 - squareOfSums limit
+
+sumOfSquares :: Fractional a => a -> a
+sumOfSquares n = n * (n + 1) / 2
+
+squareOfSums :: Fractional a => a -> a
+squareOfSums n = (n * (n + 1) * (2 * n + 1)) / 6
+
+limit :: Double
+limit = 100

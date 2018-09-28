9d8892b26c0b263e3a73c50c00941fa0b0ae6fc5
Akira Yano <akira.08280@gmail.com>
2018-09-28 16:22:45 +0900

    add euler 145 solution

---
+++ src/Euler145.hs
@@ -0,0 +1,20 @@
+{--
+  https://projecteuler.net/problem=145
+
+  Pen and Paper solution
+
+  1digit -> 0
+  2digit -> 20
+  3digit -> 100
+  4digit -> 600
+  5digit -> 0
+  6digit -> 18000
+  7digit -> 50000
+  8digit -> 540000
+  9digit -> 0
+--}
+
+module Euler145 (e145_solve) where
+
+e145_solve :: Integral a => a
+e145_solve = sum [0,20,100,600,0,18000,50000,540000,0]

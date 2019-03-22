23d84b22ee3ae7054885548013a3d95e82a9e293
Akira Yano <akira.08280@gmail.com>
2019-03-22 13:03:21 +0900

    add euler 169

---
+++ src/Euler169.hs
@@ -0,0 +1,16 @@
+{--
+  https://projecteuler.net/problem=169
+--}
+
+module Euler169 (e169Solve) where
+
+e169Solve :: Integral a => a
+e169Solve = f (10 ^ 25)
+
+f :: Integral a => a -> a
+f n = fst . f' n $ (1, 0) 
+  where
+    f' n (a, b)
+      | n == 0 = (a, b)
+      | odd n  = f' (n `div` 2) (a + b, b)
+      | even n = f' (n `div` 2) (a, a + b)

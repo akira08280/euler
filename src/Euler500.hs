762d2babf54962444b9093cb88410a90c4569330
Akira Yano <akira.08280@gmail.com>
2019-08-13 09:14:12 +0900

    refactor

--- src/Euler500.hs
+++ src/Euler500.hs
@@ -9,35 +9,35 @@ import Data.List (elemIndex)
 import Data.Numbers.Primes (primes)
 
 e500Solve :: Integral a => a
-e500Solve = foldr1 mod500500507 . take 500500 . merge $ primeTable
+e500Solve = foldr1 mod500500507 . take 500500 . sortTable $ primeTable
 
-mod500500507 :: Integral t => t -> t -> t
+mod500500507 :: Integral a => a -> a -> a
 mod500500507 a b = a * b `mod` 500500507
 
-primeTable :: Integral t => [[t]]
+primeTable :: Integral a => [[a]]
 primeTable = do
   power <- map (2 ^) [0..]
   return $ map (^ power) primes
 
-merge :: Ord a => [[a]] -> [a]
-merge = merge' 1
+sortTable :: Ord a => [[a]] -> [a]
+sortTable = sortTable' 1
   where
-    merge' k pss = min:merge' m generated
+    sortTable' k pss = min:sortTable' m generated
       where
         (i, min) = minIndex . extract (succ k) $ pss
         m = max k (succ i)
         generated = generate min pss
 
-extract :: Ord t => Int -> [[t]] -> [t]
+extract :: Ord a => Int -> [[a]] -> [a]
 extract k = map head . take k
 
-minIndex :: Ord t => [t] -> (Int, t)
+minIndex :: Ord a => [a] -> (Int, a)
 minIndex xs = (index, min)
   where
     min = minimum xs
     index = fromJust . elemIndex min $ xs
 
-generate :: Ord t => t -> [[t]] -> [[t]]
-generate m (xxs@(x:xs):yss)
+generate :: Ord a => a -> [[a]] -> [[a]]
+generate m ((x:xs):yss)
   | x == m = xs:yss
-  | otherwise = xxs:generate m yss
+  | otherwise = (x:xs):generate m yss

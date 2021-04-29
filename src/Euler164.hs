0425f5a387e43e17d7d4b4cf9149f7fc861e06ff
Akira Yano <akira.08280@gmail.com>
2021-04-29 23:11:02 +0900

    refactor

--- src/Euler164.hs
+++ src/Euler164.hs
@@ -2,20 +2,31 @@
   https://projecteuler.net/problem=164
 --}
 
-module Euler164 (e164Solve) where
+module Euler164 (e164Solve, f) where
 
 import qualified Data.Map as M
+import Data.Map.Internal
 import Data.Maybe
 
-e164Solve = sum $ [ f (19, x, 0) | x <- [1..9] ]
+e164Solve :: Integer
+e164Solve = sum $ do
+  x <- [1..9]
+  return $ f (19, x, 0)
 
-m = M.fromList [((digits, prev, prevprev), ways) | digits <- [0..20],
-                                                   prev <- [0..9],
-                                                   prevprev <- [0..9],
-                                                   let ways = f (digits, prev, prevprev) ]
+memo :: Map (Integer, Integer, Integer) Integer
+memo = M.fromList $ do
+  digits <- [0..20]
+  prev <- [0..9]
+  prevprev <- [0..9]
+  let
+    ways = f (digits, prev, prevprev)
+  return ((digits, prev, prevprev), ways)
 
 f' :: (Integer, Integer, Integer) -> Integer
-f' t = fromJust (M.lookup t m)
+f' t = fromJust (M.lookup t memo)
 
+f :: (Integer, Integer, Integer) -> Integer
 f (0, prev, prevprev) = 1
-f (n, prev, prevprev) = sum [ f' ((n-1), x, prev) | x <- [0..9 - (prev + prevprev)] ]
+f (n, prev, prevprev) = sum $ do
+  x <- [0..9 - (prev + prevprev)]
+  return $ f' (n - 1, x, prev)

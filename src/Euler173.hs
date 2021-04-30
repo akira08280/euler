b8fb88de10f9b3bb7c3b2e8ee3af71985fc161d9
Akira Yano <akira.08280@gmail.com>
2021-04-30 23:18:26 +0900

    add comment of euler 173

--- src/Euler173.hs
+++ src/Euler173.hs
@@ -1,5 +1,45 @@
 {--
   https://projecteuler.net/problem=173
+
+  n,m -> *********
+  n   -> *       *
+  n   -> *       *
+  n   -> *       *
+  n   -> *       *
+  n   -> *       *
+  n   -> *       *
+  n   -> *       *
+  n   -> *********
+
+  n,m ->  ******
+  n,m ->  ******
+  n   ->  **  **
+  n   ->  **  **
+  n   ->  ******
+  n   ->  ******
+
+  Let n be outer tiles count and let m be the outer layer count.
+  Therefore the formula to count all tiles is
+
+  f = n^2 - (n - 2m)^2 = 4m(n - m)
+  f/4m = n - m
+
+  Moreover, subtracting m from every tile count can count up every case of the same layer.
+
+  f/4m - m = n - 2m
+
+  ********* ******** ******* ****** ***** **** ***
+  *       * *      * *     * *    * *   * *  * * *
+  *       * *      * *     * *    * *   * *  * ***
+  *       * *      * *     * *    * *   * ****
+  *       * *      * *     * *    * *****
+  *       * *      * *     * ******
+  *       * *      * *******
+  *       * ********
+  *********
+
+  Finally, it is unnecessary to count up until 1000000 because one edge is not more than 1000.
+
 --}
 
 module Euler173 (e173Solve) where

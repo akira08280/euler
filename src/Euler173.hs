1656718d704c912f2fbd6b244628110e88edbe0b
Akira Yano <akira.08280@gmail.com>
2021-05-01 16:54:56 +0900

    refactor

--- src/Euler173.hs
+++ src/Euler173.hs
@@ -45,12 +45,10 @@
 module Euler173 (e173Solve) where
 
 e173Solve :: Integer
-e173Solve = sum . map (\x -> m `div` x - x) $ [1..upper]
-  where
-    upper = ceiling . sqrt . fromIntegral $ m
+e173Solve = sum . map (\m -> tiles `div` (4 * m) - m) $ [1..upper]
 
-m :: Integral a => a
-m = tiles `div` 4 
+upper :: Integer
+upper = ceiling . sqrt . fromIntegral $ tiles `div` 4
 
 tiles :: Integral a => a
 tiles = 10 ^ 6

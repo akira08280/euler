d627b5eab5dbfd00f42f0ffd4f5f4e723228d9be
Akira Yano <akira.08280@gmail.com>
2021-05-01 23:52:15 +0900

    add euler 174

---
+++ src/Euler174.hs
@@ -0,0 +1,19 @@
+{--
+  https://projecteuler.net/problem=174
+--}
+
+module Euler174 (e174Solve, f) where
+
+import Data.List (sort, group)
+
+e174Solve :: Int
+e174Solve = length . filter (< 11) . map length . group . sort $ f
+
+f :: [Integer]
+f = do
+  k <- [1..500]
+  let
+    start = succ k
+    upper = 250000 `div` k
+  j <- [start..upper]
+  return $ k * j

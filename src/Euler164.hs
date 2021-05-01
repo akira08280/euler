587bf561f4be74db65fbce4251fea4b34a7c0083
Akira Yano <akira.08280@gmail.com>
2021-05-01 22:02:13 +0900

    refactor

--- src/Euler164.hs
+++ src/Euler164.hs
@@ -2,7 +2,7 @@
   https://projecteuler.net/problem=164
 --}
 
-module Euler164 (e164Solve, f) where
+module Euler164 (e164Solve) where
 
 import qualified Data.Map as M
 import Data.Map.Internal
@@ -13,6 +13,12 @@ e164Solve = sum $ do
   x <- [1..9]
   return $ f (19, x, 0)
 
+f :: (Integer, Integer, Integer) -> Integer
+f (0, prev, prevprev) = 1
+f (n, prev, prevprev) = sum $ do
+  x <- [0..9 - (prev + prevprev)]
+  return $ fromJust (M.lookup (n - 1, x, prev) memo)
+
 memo :: Map (Integer, Integer, Integer) Integer
 memo = M.fromList $ do
   digits <- [0..19]
@@ -21,12 +27,3 @@ memo = M.fromList $ do
   let
     ways = f (digits, prev, prevprev)
   return ((digits, prev, prevprev), ways)
-
-f' :: (Integer, Integer, Integer) -> Integer
-f' t = fromJust (M.lookup t memo)
-
-f :: (Integer, Integer, Integer) -> Integer
-f (0, prev, prevprev) = 1
-f (n, prev, prevprev) = sum $ do
-  x <- [0..9 - (prev + prevprev)]
-  return $ f' (n - 1, x, prev)

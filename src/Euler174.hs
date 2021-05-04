9e84d92d68f43b24f650c7abbb42217d80943730
Akira Yano <akira.08280@gmail.com>
2021-05-04 11:50:24 +0900

    ad comment

--- src/Euler174.hs
+++ src/Euler174.hs
@@ -1,5 +1,16 @@
 {--
   https://projecteuler.net/problem=174
+
+  Let n be outer tiles count and let m be the outer layer count.
+  Therefore the formula to count all tiles is
+
+  t = 1000000 = n^2 - (n - 2m)^2 = 4m(n - m)
+  t/4 = 250000 = m(n - m) and 2m < n
+
+  So,
+
+  m(n - m) < 250000 and n - m > m
+
 --}
 
 module Euler174 (e174Solve) where

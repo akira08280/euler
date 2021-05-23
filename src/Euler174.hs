bfb5804a7fd1d2294a3dc3febe44f3bcfe67dbe6
Akira Yano <akira.08280@gmail.com>
2021-05-23 09:58:56 +0900

    refactor

--- src/Euler174.hs
+++ src/Euler174.hs
@@ -1,16 +1,5 @@
 {--
   https://projecteuler.net/problem=174
-
-  Let n be outer tiles count and let m be the outer layer count.
-  Therefore the formula to count all tiles is
-
-  t = 1000000 = n^2 - (n - 2m)^2 = 4m(n - m)
-  t/4 = 250000 = m(n - m) and 2m < n
-
-  So,
-
-  m(n - m) < 250000 and n - m > m
-
 --}
 
 module Euler174 (e174Solve) where
@@ -19,9 +8,9 @@ import Data.List (sort, group)
 
 e174Solve :: Int
 e174Solve = length . filter (< 11) . map length . group . sort $ do
-  k <- [1..500]
+  m <- [1..499]
   let
-    start = succ k
-    upper = 250000 `div` k
-  j <- [start..upper]
-  return $ k * j
+    start = succ m
+    upper = 250000 `div` m
+  n_m <- [start..upper]
+  return $ m * n_m
